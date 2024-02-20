2132704aa60a1abb11fe32cfe13cd167807c5d01
Young Jin <yjsongaa@gmail.com>
2024-02-20 15:05:53 -0600

    Table 01_in progress

---
+++ src/Table01.py
@@ -0,0 +1,136 @@
+import pandas as pd
+import numpy as np
+import datetime as datetime
+
+import config
+from pathlib import Path
+DATA_DIR = Path(config.DATA_DIR)
+OUTPUT_DIR = Path(config.OUTPUT_DIR)
+
+
+import load_nyfed
+
+df_2000s = load_nyfed.load_nyfed(url=load_nyfed.url, data_dir=DATA_DIR, 
+                           save_cache=False, sheet_name = '2000s')
+df_2000s = df_2000s.drop(index=[0,1])
+df_2000s.reset_index(drop=True, inplace=True)
+
+df_2000s.columns = df_2000s.iloc[0]
+df_2000s.drop(index=0, inplace=True)
+df_2000s.reset_index(drop=True, inplace=True)
+df_2014 = df_2000s.iloc[:22,14].to_frame(name='Primary Dealer')
+
+
+df_dealer_alpha = load_nyfed.load_nyfed(url=load_nyfed.url, data_dir=DATA_DIR, 
+                           save_cache=False, sheet_name = 'Dealer Alpha')
+df_dealer_alpha.drop(index=0, inplace=True)
+df_dealer_alpha.reset_index(drop=True, inplace=True)
+
+df_dealer_alpha.columns = df_dealer_alpha.iloc[0]
+df_dealer_alpha.drop(index=0,inplace=True)
+df_dealer_alpha = df_dealer_alpha.iloc[:,:3]
+df_dealer_alpha.reset_index(drop=True, inplace=True)
+
+df_dealer_alpha['Start Date'] = pd.to_datetime(df_dealer_alpha['Start Date'])
+df_dealer_alpha['Start Date'] = df_dealer_alpha['Start Date'].dt.strftime('%-m/%-d/%Y')
+df_dealer_alpha['End Date'] = pd.to_datetime(df_dealer_alpha['End Date'], errors='coerce')
+df_dealer_alpha['End Date'] = df_dealer_alpha['End Date'].apply(lambda x: x.date() 
+                                                                if pd.notnull(x) else x)
+df_dealer_alpha['End Date'] = df_dealer_alpha['End Date'].fillna('Current')
+
+
+df_2014['Primary_Dealer_Temp'] = df_2014['Primary Dealer'].str.strip().str.replace(r'\.$', '', regex=True).str.replace(' ', '')
+df_dealer_alpha['Primary_Dealer_Temp'] = df_dealer_alpha['Primary Dealer'].str.strip().str.replace(r'\.$', '', regex=True).str.replace(' ', '')
+df_dealer_alpha = df_dealer_alpha.sort_values(by='Start Date')
+df_dealer_alpha = df_dealer_alpha.drop_duplicates(subset='Primary_Dealer_Temp', keep='last')
+merged_df = pd.merge(df_2014, df_dealer_alpha[['Primary_Dealer_Temp', 'Start Date']],
+                    on='Primary_Dealer_Temp', how='left')
+merged_df.drop(columns=['Primary_Dealer_Temp'],inplace=True)
+merged_df['Start Date'] = pd.to_datetime(merged_df['Start Date'])
+merged_df.sort_values(by='Start Date', inplace=True)
+blankIndex=[''] * len(merged_df)
+merged_df.index=blankIndex
+merged_df
+
+
+
+
+
+df_level = load_fred.load_fred(data_dir=DATA_DIR).dropna()
+
+df_quarterly = 100 * df_level.pct_change()
+# df_quarterly.plot()
+
+# Select only the values that occur in July
+_df = df_level[df_level.index.month == 7]
+df_annual = 100 * _df.pct_change()
+# df_annual.plot()
+
+df_quarterly.describe()
+df_annual.describe()
+
+
+columns_for_summary_stats = [
+    'CPIAUCNS',
+    'GDPC1',
+    ]
+
+# This maps the column names to their LaTeX descriptions
+column_names_map = {
+    'CPIAUCNS':'Inflation',
+    'GDPC1':'Real GDP',
+}
+
+escape_coverter = {
+    '25%':'25\\%',
+    '50%':'50\\%',
+    '75%':'75\\%'
+}
+
+df_annual = df_annual[columns_for_summary_stats]
+
+## Suppress scientific notation and limit to 3 decimal places
+# Sets display, but doesn't affect formatting to LaTeX
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+# Sets format for printing to LaTeX
+float_format_func = lambda x: '{:.2f}'.format(x)
+
+# Pooled summary stats
+describe_all = (
+    df_annual[columns_for_summary_stats].
+    describe().T.
+    rename(index=column_names_map, columns=escape_coverter)
+)
+describe_all['count'] = describe_all['count'].astype(int)
+describe_all.columns.name = 'Full Sample: 1947 - 2023'
+latex_table_string_all = describe_all.to_latex(escape=False, float_format=float_format_func)
+
+describe1 = (
+    df_annual[columns_for_summary_stats].
+    describe().T.
+    rename(index=column_names_map, columns=escape_coverter)
+)
+describe1['count'] = describe1['count'].astype(int)
+describe1.columns.name = 'Subsample: 1947 - 1990'
+latex_table_string1 = describe1.to_latex(escape=False, float_format=float_format_func)
+
+describe2 = (
+    df_annual.loc["1990":,columns_for_summary_stats].
+    describe().T.
+    rename(index=column_names_map, columns=escape_coverter)
+)
+describe2.columns.name = 'Subsample: 1990-2023'
+latex_table_string2 = describe2.to_latex(escape=False, float_format=float_format_func)
+
+latex_table_string_split = [
+    *latex_table_string_all.split('\n')[0:-3], # Skip the \end{tabular} and \bottomrule lines
+    '\\midrule',
+    *latex_table_string1.split('\n')[2:-3], # Skip the \begin and \end lines
+    '\\midrule',
+    *latex_table_string2.split('\n')[2:] # Skip the \begin{tabular} and \toprule lines
+]
+latex_table_string = '\n'.join(latex_table_string_split)
+# print(latex_table_string)
+path = OUTPUT_DIR / f'example_table.tex'
+with open(path, "w") as text_file:
+    text_file.write(latex_table_string)
--- src/load_nyfed.py
+++ src/load_nyfed.py
@@ -10,29 +10,63 @@ def load_nyfed(
         url=url,
         data_dir=DATA_DIR,
         from_cache=True,
-        save_cache=False
+        save_cache=False,
+        sheet_name=None
         ):
     """
     Load NY Fed primary dealers list from NYFed website or from cache.
     """
+    file_path = data_dir / "pulled" / "nyfed_primary_dealers_list.xls"
+    print(f"Attempting to load from: {file_path}")
     
-    if from_cache:
-        file_path = Path(data_dir) / "pulled" / "nyfed_primary_dealers_list.xls"
-        df = pd.read_excel(file_path)
+    if from_cache and file_path.exists():
+        print("Loading from cache.")
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
     else:
+        print(f"File not found in cache or from_cache set to False. Downloading from {url}")
         try:
-            df = pd.read_excel(url)
+            df = pd.read_excel(url, sheet_name=sheet_name)
             if save_cache:
-                file_dir = Path(data_dir) / "pulled"
-                file_dir.mkdir(parents=True, exist_ok=True)
-                df.to_excel(file_dir / "nyfed_primary_dealers_list.xls")
+                file_path.parent.mkdir(parents=True, exist_ok=True)  # Ensure directory exists
+                df.to_excel(file_path, index=False)
+                print(f"File saved to cache at {file_path}.")
         except Exception as e:
-            print("Error loading Excel file:", e)
+            print(f"Error loading Excel file: {e}")
+            raise
     
     return df
 
 
 if __name__ == "__main__":
     # Pull and save cache of NYFED primary dealers list data
-    _ = load_nyfed(
-        url=url, data_dir=DATA_DIR, from_cache=False, save_cache=True)
+    df = load_nyfed(url=url, data_dir=DATA_DIR, from_cache=False, save_cache=True)
+    print(df.head())  # Display the first few rows of the dataframe
+
+
+
+
+
+# def load_nyfed(url=url, data_dir=DATA_DIR, save_cache=False):
+#     """
+#     Directly download NY Fed primary dealers list from NYFed website and optionally save to cache.
+#     """
+#     file_path = data_dir / "pulled" / "nyfed_primary_dealers_list.xls"
+    
+#     print(f"Downloading from {url}")
+#     try:
+#         df = pd.read_excel(url)
+#         if save_cache:
+#             file_path.parent.mkdir(parents=True, exist_ok=True)  # Ensure directory exists
+#             df.to_excel(file_path, index=False)
+#             print(f"File saved to cache at {file_path}.")
+#     except Exception as e:
+#         print(f"Error loading Excel file: {e}")
+#         raise
+    
+#     return df
+
+
+# if __name__ == "__main__":
+#     # Always download and optionally save the cache of NYFED primary dealers list data
+#     df = load_nyfed.load_nyfed(url=url, data_dir=DATA_DIR, save_cache=True)
+#     print(df.head())  # Display the first few rows of the dataframe
